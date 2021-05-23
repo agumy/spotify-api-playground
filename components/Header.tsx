7f553ed0c02235ffdf267d359f69191cc1195167
agumy <nov13sh@gmail.com>
2021-05-23 16:09:14 +0900

    add h1 title

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,8 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <div className="h-24 bg-gray-800 flex items-center p-5 border-b border-gray-400">
+    <div className="h-24 bg-gray-800 flex items-center justify-between p-5 border-b border-gray-400">
+      <h1 className="text-xl text-gray-100">Spotify API Playground</h1>
       <Link href="/login">
         <span className="text-gray-400 h-6 p-2 flex items-center justify-center hover:text-white cursor-pointer">
           Login Spotify
